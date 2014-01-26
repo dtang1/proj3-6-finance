346f8a397349ed11c0eff323d03ace10a0b45a38
Jasper Lu <repsajrules@gmail.com>
2014-01-26 15:23:30 -0800

    added fb lgoin js

--- client/login.js
+++ client/login.js
@@ -1,7 +1,18 @@
 Template.login.events({
     'click #google-login': function() {
-        Meteor.loginWithGoogle(function (){
-           Router.go("/app");    
+        Meteor.loginWithGoogle(function (e,r){
+            if(e)
+                alert(e);
+            else
+                Router.go("/app");    
+        });
+    },
+    'click #fb-login': function() {
+        Meteor.loginWithFacebook(function (e,r){
+            if(e)
+                alert(e);
+            else
+                Router.go("/app");    
         });
     }
 });
