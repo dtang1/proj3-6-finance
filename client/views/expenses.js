604c72e5ced2c68e88a71748937a5874f1eb46e0
Jasper Lu <repsajrules@gmail.com>
2014-01-28 00:01:54 -0800

    whoops forgot to commit the magical js for the expenses page

---
+++ client/views/expenses.js
@@ -0,0 +1,15 @@
+Template.expenseCards.foodTransacs = function() {
+   return Transactions.find({userId:Meteor.userId(),category:'Food'},{sort: {date: -1}}).fetch(); 
+}
+
+Template.expenseCards.clothesTransacs = function() {
+   return Transactions.find({userId:Meteor.userId(),category:'Clothing'},{sort: {date: -1}}).fetch(); 
+}
+
+Template.expenseCards.enterTransacs = function() {
+   return Transactions.find({userId:Meteor.userId(),category:'Entertainment'},{sort: {date: -1}}).fetch(); 
+}
+
+Template.expenseCards.otherTransacs = function() {
+   return Transactions.find({userId:Meteor.userId(),category:'Other'},{sort: {date: -1}}).fetch(); 
+}
