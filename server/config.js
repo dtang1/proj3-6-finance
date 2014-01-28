e7ad45d51ecf57141c28ff7e40ddb279dc9d814d
Steve Zhu <st.zhu1@gmail.com>
2014-01-27 20:45:12 -0500

    Config

--- server/config.js
+++ server/config.js
@@ -1,104 +1,83 @@
-isProdEnv = function () {
-	return process.env.ROOT_URL != "http://localhost:3000";
-}
+log.info("root_url: " + process.env.ROOT_URL);
 
-Accounts.loginServiceConfiguration.remove({
-	service: 'google'
-});
+Meteor.startup(function() {
+	Accounts.loginServiceConfiguration.remove({
+		service: 'google'
+	});
+	Accounts.loginServiceConfiguration.remove({
+		service: 'facebook'
+	});
+	Accounts.loginServiceConfiguration.remove({
+		service: 'github'
+	});
 
-Accounts.loginServiceConfiguration.remove({
-	service: 'facebook'
-});
-
-Accounts.loginServiceConfiguration.remove({
-	service: 'github'
-});
-
-if (isProdEnv()) {
 	Accounts.loginServiceConfiguration.insert({
 		service: 'github',
-		clientId: 'e7edba365fb5d439ae18',
-		secret: '73dd43c033c6ba9bbd5c9dccf679def47dac96d1'
+		clientId: '5f40590c7972a395108f',
+		secret: '936e6c8cb3590f457f4262801c428a952ad1a666'
 	});
 	Accounts.loginServiceConfiguration.insert({
 		service: 'google',
-		clientId: '541914321.apps.googleusercontent.com',
-		secret: 'PKCLBjlMyMrLge-YGbrI_U8i'
+		clientId: '749382359180-74ui89rp3lhko3qr3pgp00d7vf8dk9j0.apps.googleusercontent.com',
+		secret: 'SUNW4EFXqKSYynPoO00y75jQ'
 	});
 	Accounts.loginServiceConfiguration.insert({
 		service: 'facebook',
 		appId: '701125876588992',
 		secret: 'f6616315379c9151de113a61cb330215'
 	});
-} else {
-	// dev environment
-	Accounts.loginServiceConfiguration.insert({
-		service: 'github',
-		clientId: '16159c6651c71bc5ce53',
-		secret: 'e635421781992ff5af0141d556e8cf78471fc1c2'
-	});
-	Accounts.loginServiceConfiguration.insert({
-		service: 'google',
-		clientId: '541914321.apps.googleusercontent.com',
-		secret: 'PKCLBjlMyMrLge-YGbrI_U8i'
-	});
-	Accounts.loginServiceConfiguration.insert({
-		service: 'facebook',
-		appId: '701125876588992',
-		secret: 'f6616315379c9151de113a61cb330215'
-	});
-}
 
-Accounts.onCreateUser(function (options, user) {
-	if (user.services) {
-		if (options.profile) {
-			user.profile = options.profile
-		}
-		var service = _.keys(user.services)[0];
-		var email = user.services[service].email;
-		if (!email && user.emails) {
-			email = user.emails.address;
-		}
-		if (!email) {
-			email = options.email;
-		}
-		if (!email) {
-			// if email is not set, there is no way to link it with other accounts
-			return user;
-		}
-
-		// see if any existing user has this email address, otherwise create new
-		var existingUser = Meteor.users.findOne({'emails.address': email});
-		if (!existingUser) {
-			// check for email also in other services
-			var existingGitHubUser = Meteor.users.findOne({'services.github.email': email});
-			var existingGoogleUser = Meteor.users.findOne({'services.google.email': email});
-			var existingFacebookUser = Meteor.users.findOne({'services.facebook.email': email});
-			var doesntExist = !existingGitHubUser && !existingGoogleUser && !existingFacebookUser;
-			if (doesntExist) {
-				// return the user as it came, because there he doesn't exist in the DB yet
+	Accounts.onCreateUser(function (options, user) {
+		if (user.services) {
+			if (options.profile) {
+				user.profile = options.profile
+			}
+			var service = _.keys(user.services)[0];
+			var email = user.services[service].email;
+			if (!email && user.emails) {
+				email = user.emails.address;
+			}
+			if (!email) {
+				email = options.email;
+			}
+			if (!email) {
+				// if email is not set, there is no way to link it with other accounts
 				return user;
-			} else {
-				existingUser = existingGitHubUser || existingGoogleUser || existingFacebookUser;
-				if (existingUser && user.emails) {
-					existingUser.emails = user.emails;
+			}
+
+			// see if any existing user has this email address, otherwise create new
+			var existingUser = Meteor.users.findOne({'emails.address': email});
+			if (!existingUser) {
+				// check for email also in other services
+				var existingGitHubUser = Meteor.users.findOne({'services.github.email': email});
+				var existingGoogleUser = Meteor.users.findOne({'services.google.email': email});
+				var existingFacebookUser = Meteor.users.findOne({'services.facebook.email': email});
+				var doesntExist = !existingGitHubUser && !existingGoogleUser && !existingFacebookUser;
+				if (doesntExist) {
+					// return the user as it came, because there he doesn't exist in the DB yet
+					return user;
+				} else {
+					existingUser = existingGitHubUser || existingGoogleUser || existingFacebookUser;
+					if (existingUser && user.emails) {
+						existingUser.emails = user.emails;
+					}
 				}
 			}
+
+			// precaution, these will exist from accounts-password if used
+			if (!existingUser.services) {
+				existingUser.services = { resume: { loginTokens: [] }};
+			}
+
+			// copy across new service info
+			existingUser.services[service] = user.services[service];
+			existingUser.services.resume.loginTokens.push(
+				user.services.resume.loginTokens[0]
+			);
+
+			// even worse hackery
+			Meteor.users.remove({_id: existingUser._id}); // remove existing record
+			return existingUser;    		      // record is re-inserted
 		}
-
-		// precaution, these will exist from accounts-password if used
-		if (!existingUser.services) {
-			existingUser.services = { resume: { loginTokens: [] }};
-		}
-
-		// copy across new service info
-		existingUser.services[service] = user.services[service];
-		existingUser.services.resume.loginTokens.push(
-			user.services.resume.loginTokens[0]
-		);
-
-		// even worse hackery
-		Meteor.users.remove({_id: existingUser._id}); // remove existing record
-		return existingUser;    		      // record is re-inserted
-	}
+	});
 });
